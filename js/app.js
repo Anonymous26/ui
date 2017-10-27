2fdba12b6888a6061d8b121fdc94f9fd1dbd6671
Piyush Agrawal <poush12@gmail.com>
2017-10-28 01:07:56 +0530

    Login actions

--- js/app.js
+++ js/app.js
@@ -1,6 +1,22 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    doc: false,
+    pat: false,
+    hos: false
+  },
+  methods: {
+  	update: function(val){
+  		this.doc = this.pat = this.hos = false;
+  		console.log(val)
+  		switch(val){
+  			case 1: this.hos = true;
+  			break;
+  			case 2: this.pat = true;
+  			break;
+  			case 0: this.doc = true;
+  			break;
+  		}
+  	}
   }
 })
