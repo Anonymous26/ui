07bae02fd605b6650aa1098cfa65050e1db2a234
Piyush Agrawal <poush12@gmail.com>
2017-10-28 04:09:02 +0530

    something

--- js/app.js
+++ js/app.js
@@ -3,7 +3,11 @@ var app = new Vue({
   data: {
     doc: false,
     pat: false,
-    hos: false
+    hos: false,
+    busy: false,
+    user: 'John',
+    uploadFieldName: '',
+    files: ['as', 'sd', 'sds']
   },
   methods: {
   	update: function(val){
@@ -17,6 +21,31 @@ var app = new Vue({
   			case 0: this.doc = true;
   			break;
   		}
-  	}
+  	},
+  	processFile: function(event) {
+  		console.log(event)
+    	this.uploadFieldName = event.target.files[0]
+  	},
+  	loadMore: function() {
+  		console.log("Sdsd")	
+    }
   }
 })
+
+
+// (function ($) {
+// $('#uploadit').on('click', function () {
+//   var file = $('#file')[0].files[0];
+//   var fileReader = new FileReader();
+//   fileReader.onloadend = function (e) {
+//     var arrayBuffer = e.target.result;
+//     var fileType = $('#file-type').val();
+//     blobUtil.arrayBufferToBlob(arrayBuffer, fileType).then(function (blob) {
+//       console.log('here is a blob', blob);
+//       console.log('its size is', blob.size);
+//       console.log('its type is', blob.type);
+//     }).catch(console.log.bind(console));
+//   };
+//   fileReader.readAsArrayBuffer(file);
+// });
+// })(jQuery);
